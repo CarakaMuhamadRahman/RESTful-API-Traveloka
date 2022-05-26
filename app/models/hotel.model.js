1bbc32355edf531003f5a1f84ef62cebd8dc05ae
colab-CSD-035 <dandi.ir20@gmail.com>
2022-05-27 05:16:10 +0700

    Update find a hotel by id and retrieve a single object find by id

--- app/models/hotel.model.js
+++ app/models/hotel.model.js
@@ -19,6 +19,23 @@ Hotel.create = (newHotel, result) => {
     });
 };
 
+hotel.findById = (id, result) => {
+    sql.query(`SELECT * FROM hotel WHERE id = "${id}"`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+        if (res.length) {
+            console.log("found Hotel ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+        // not found Hotel with the id
+        result({kind: "not_found" }, null);
+    });
+};
+
 Hotel.getAll = (name, result) => {
     let query = "SELECT * FROM hotel";
     if (name) {
