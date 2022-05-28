2cc7dd2a47ffdc5d98eeb1293d27bc611975af6d
CarakaMuhamadRahman <cmr220821@gmail.com>
2022-05-28 20:17:33 +0700

    update origin

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: ["*"]
+  origin: '*'
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
